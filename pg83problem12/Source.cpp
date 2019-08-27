5d0b13316c48668a98c87558a1debef6eae3e997
JSG1987 <JGentry0058@gmail.com>
2019-08-27 15:18:16 -0500

    HW12

---
+++ pg83problem12/Source.cpp
@@ -0,0 +1,22 @@
+//The goal of this program is to calculate the acrege of 391,876 square feet 
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int acre = 43560;
+	int land = 391876;
+	float convert = land / acre;
+	cout << "391,876 square feet is " << convert << " acres\n";
+
+
+
+
+
+
+	system("PAUSE");
+	return 0;
+}
